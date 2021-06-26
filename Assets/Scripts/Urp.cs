85ea6feb808a31682466f65c76e70bd625798a4b
Daniele Comotti <comotti.daniele@gmail.com>
2021-06-26 19:02:28 +0200

    Small fixes

--- Assets/Scripts/Urp.cs
+++ Assets/Scripts/Urp.cs
@@ -1,12 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class Urp : MonoBehaviour
 {
+    //public Volume volume;
+    //Vignette vignette;
     // Start is called before the first frame update
     void Start()
     {
+        //if(volume.profile.TryGet(out Vignette vignette))
+        //{
+        //    this.vignette = vignette;
+        //}
         Destroy(gameObject, 0.2f);
     }
 
